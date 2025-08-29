5365b92cf957e75e7b527ad787b645e927b54e56
Zoro7982 <sachansameer8@outlook.com>
2025-08-29 17:58:16 +0530

    bookbot complete

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
-from stats import count_words
-from stats import num_words
+from stats import count_words,num_words,char_dict_to_sorted_list
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def main():
-    filepath="books/frankenstein.txt"
+    filepath=sys.argv[1]
     text=get_book_text(filepath)
-    return print(f"{count_words(text)} words found in the document"),print(num_words(text))
+    word_count=count_words(text)
+    char_dict=num_words(text)
+    char_sorted_list=char_dict_to_sorted_list(char_dict)
+    print_report(filepath,word_count,char_sorted_list)
 
 def get_book_text(filepath):
     with open(filepath) as f:
         text=f.read()
     return text
 
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
 main()
